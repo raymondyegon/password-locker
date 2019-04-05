79c4843b7bfc93f02eb9625d6cc9b24dcc45936c
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-05 11:25:22 +0300

    A test to check new instance of user is tested and working

--- test_locker.py
+++ test_locker.py
@@ -1,2 +1,13 @@
 import unittest
 from locker import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class.
+    '''
+    
+    def setUp(self):
+        self.new_user = User("Raymond", "0222d082njknjdk")
+    
+    def test_init(self):
+        assert self.new_user.username == "Raymond"
