e1bbf28a40c0fbf9467dcf9e00fad36c636ee2e7
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-05 12:13:39 +0300

    Test case to check whether multiple users can register passed

--- test_locker.py
+++ test_locker.py
@@ -14,7 +14,7 @@ class TestUser(unittest.TestCase):
         '''
         Teardown method that does clean up after each test case has run
         '''
-        
+
         User.users_list = []
 
     def test_init(self):
@@ -28,6 +28,21 @@ class TestUser(unittest.TestCase):
         '''
         test_register_user test case to test if a user is registered in the users list
         '''
-        self.new_user.register_user() # registering user
-        
+        self.new_user.register_user()  # registering user
+
         assert len(User.users_list) == 1
+
+    def test_register_multiple_user(self):
+        '''
+        test_register_multiple_user test case to register multiple users
+        '''
+        self.new_user.register_user()
+        test_user = User("monday", "0934idn")
+        
+        test_user.register_user()
+        assert len(User.users_list) == 2
+
+    def test_delete_user(self):
+        '''
+        test_delete_user test case to test if a user can delete 
+        '''
