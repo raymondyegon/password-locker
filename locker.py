794e2703dde8fc185b1893a6ccac08cddbadfd51
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-05 11:34:44 +0300

    Test function to write user has failed

--- locker.py
+++ locker.py
@@ -9,3 +9,4 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+    
--- test_locker.py
+++ test_locker.py
@@ -1,13 +1,33 @@
 import unittest
 from locker import User
 
+
 class TestUser(unittest.TestCase):
     '''
     Test class that defines test cases for the user class.
     '''
-    
+
     def setUp(self):
         self.new_user = User("Raymond", "0222d082njknjdk")
-    
+
+    def tearDown(self):
+        '''
+        Teardown method that does clean up after each test case has run
+        '''
+        
+        User.users_list = []
+
     def test_init(self):
+        '''
+        test_init test case to test if the object is initialized
+        '''
         assert self.new_user.username == "Raymond"
+        assert self.new_user.password == "0222d082njknjdk"
+
+    def test_register_user(self):
+        '''
+        test_register_user test case to test if a user is registered in the users list
+        '''
+        self.new_user.register_user() # registering user
+        
+        assert len(User.users_list) == 1
