f800a38a34a5ea50dbab5494523991f7691288ef
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-08 08:53:53 +0300

    All test are running and have passed

--- test_locker.py
+++ test_locker.py
@@ -1,125 +1,139 @@
 import unittest
-from locker import User
-from locker import Credentials
+import pyperclip
+from locker import User, Credential
 
 
 class TestUser(unittest.TestCase):
     '''
-    Test class that defines test cases for the user class.
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: helps in creating test cases
     '''
 
     def setUp(self):
-        self.new_user = User("Raymond", "0222d082njknjdk")
-
-    def tearDown(self):
         '''
-        Teardown method that does clean up after each test case has run
+        Function to create a user account before each test
         '''
+        self.new_user = User('Mary', 'Ng\'ang\'a', 'pswd100')
 
-        User.users_list = {}
-
-    def test_init(self):
+    def test__init__(self):
         '''
-        test_init test case to test if the object is initialized
+        Test to if check the initialization/creation of user instances is properly done
         '''
-        assert self.new_user.username == "Raymond"
-        assert self.new_user.password == "0222d082njknjdk"
+        self.assertEqual(self.new_user.first_name, 'Mary')
+        self.assertEqual(self.new_user.last_name, 'Ng\'ang\'a')
+        self.assertEqual(self.new_user.password, 'pswd100')
 
-    def test_register_user(self):
+    def test_save_user(self):
         '''
-        test_register_user test case to test if a user is registered in the users list
+        Test to check if the new users info is saved into the users list
         '''
-        self.new_user.register_user()  # registering user
-
-        assert len(User.users_list) == 1
-
-    def test_register_multiple_user(self):
-        '''
-        test_register_multiple_user test case to register multiple users
-        '''
-        self.new_user.register_user()
-        test_user = User("monday", "0934idn")
-
-        test_user.register_user()
-        assert len(User.users_list) == 2
-
-    # def test_delete_user(self):
-    #     '''
-    #     test_delete_user test case to test if a user can delete
-    #     '''
-    #     self.new_user.register_user()
-    #     test_user = User("monday", "12345678")
-
-    #     test_user.register_user()
-
-    #     User.delete_user("Raymond")  # deleting a user
-
-    #     assert len(User.users_list) == 1
-
-    def test_user_exist(self):
-        '''
-        test_user_exist test case to return boolean if we cannot find a user
-        '''
-        self.new_user.register_user()
-        test_user = User("monday", "w12345678")
-        test_user.register_user()
-
-        user_exists = User.user_exist("monday")
-
-        assert True == user_exists
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list), 1)
 
 
 class TestCredentials(unittest.TestCase):
     '''
-    Test class that defines test cases for the user class.
+    Test class that defines test cases for the credentials class behaviours.
+    Args:
+        unittest.TestCase: helps in creating test cases
     '''
 
+    def test_check_user(self):
+        '''
+        Function to test whether the login in function check_user works as expected
+        '''
+        self.new_user = User('Mary', 'Ng\'ang\'a', 'pswd100')
+        self.new_user.save_user()
+        user2 = User('Ken', 'Ng\'ang\'a', 'pswd100')
+        user2.save_user()
+
+        for user in User.users_list:
+            if user.first_name == user2.first_name and user.password == user2.password:
+                current_user = user.first_name
+        return current_user
+
+        self.assertEqual(current_user, Credential.check_user(
+            user2.password, user2.first_name))
+
     def setUp(self):
-        self.new_credentials = Credentials("facebook")
+        '''
+        Function to create an account's credentials before each test
+        '''
+        self.new_credential = Credential(
+            'Mary', 'Facebook', 'maryjoe', 'pswd100')
+
+    def test__init__(self):
+        '''
+        Test to if check the initialization/creation of credential instances is properly done
+        '''
+        self.assertEqual(self.new_credential.user_name, 'Mary')
+        self.assertEqual(self.new_credential.site_name, 'Facebook')
+        self.assertEqual(self.new_credential.account_name, 'maryjoe')
+        self.assertEqual(self.new_credential.password, 'pswd100')
+
+    def test_save_credentials(self):
+        '''
+        Test to check if the new credential info is saved into the credentials list
+        '''
+        self.new_credential.save_credentials()
+        twitter = Credential('Jane', 'Twitter', 'maryjoe', 'pswd100')
+        twitter.save_credentials()
+        self.assertEqual(len(Credential.credentials_list), 2)
+
+    # def test_generate_password(self):
+    # 	'''
+    # 	Test to check if the generate password generates 8 character long alphanumeric numbers
+    # 	'''
+    # 	self.twitter = Credential('Twitter','maryjoe','')
+    # 	self.twitter.password = generate_password()
+    # 	self.assertEqual()
 
     def tearDown(self):
         '''
-        Teardown method that does clean up after each test case has run
+        Function to clear the credentials list after every test
         '''
-        Credentials.credentials_list = {}
-        Credentials.platforms = []
+        Credential.credentials_list = []
+        User.users_list = []
 
-    def test_init(self):
+    def test_display_credentials(self):
         '''
-        test_init test case to test if the object is initialized
+        Test to check if the display_credentials method, displays the correct credentials.
         '''
-        assert self.new_credentials.account == "facebook"
+        self.new_credential.save_credentials()
+        twitter = Credential('Jane', 'Twitter', 'maryjoe', 'pswd100')
+        twitter.save_credentials()
+        gmail = Credential('Jane', 'Gmail', 'maryjoe', 'pswd200')
+        gmail.save_credentials()
+        self.assertEqual(
+            len(Credential.display_credentials(twitter.user_name)), 2)
 
-    def test_register_platform(self):
+    def test_find_by_site_name(self):
         '''
-        test_register_platform to test if a platform is registered
+        Test to check if the find_by_site_name method returns the correct credential
         '''
-       
-        self.new_credentials.register_platform()
+        self.new_credential.save_credentials()
+        twitter = Credential('Jane', 'Twitter', 'maryjoe', 'pswd100')
+        twitter.save_credentials()
+        credential_exists = Credential.find_by_site_name('Twitter')
+        self.assertEqual(credential_exists, twitter)
 
-        self.assertEqual(len(Credentials.platforms), 1)
+    def test_copy_credential(self):
+        '''
+        Test to check if the copy a credential method copies the correct credential
+        '''
+        self.new_credential.save_credentials()
+        twitter = Credential('Jane', 'Twitter', 'maryjoe', 'pswd100')
+        twitter.save_credentials()
+        find_credential = None
+        for credential in Credential.user_credentials_list:
+            find_credential = Credential.find_by_site_name(
+                credential.site_name)
+            return pyperclip.copy(find_credential.password)
+        Credential.copy_credential(self.new_credential.site_name)
+        self.assertEqual('pswd100', pyperclip.paste())
+        print(pyperclip.paste())
 
-    def test_register_multiple_platforms(self):
-        '''
-        test_register_multiple_platforms to test if multiple platforms can be registered
-        '''
-        self.new_credentials.register_platform()
-        test_platform = Credentials("Instagram")
-        
-        test_platform.register_platform()
-        
-        assert len(Credentials.platforms) == 2
-    
-    def test_platform_delete(self):
-        '''
-        test_platform_delete to test if a platform can be deleted.
-        '''
-        self.new_credentials.register_platform()
-        test_platform = Credentials("Instagram")
-        
-        test_platform.register_platform()
-        
-        self.new_credentials.delete_platform()
-        
-        assert len(Credentials.platforms) == 1
-        
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
