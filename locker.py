779742443e4330bd8d2d7333b4b4188045cc7308
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-08 09:11:46 +0300

    All necessary functions added and test cases all passed

--- locker.py
+++ locker.py
@@ -1,69 +1,104 @@
-#!/usr/gin/env python3
+import pyperclip
+import random
+import string
+
+# Global Variables
+global users_list
 
 
 class User:
     '''
-    Class that generates new instances of user
+    Class to create user accounts and save their information
     '''
-    users_list = {}
+    # Class Variables
+    # global users_list
+    users_list = []
 
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, first_name, last_name, password):
+        '''
+        Method to define the properties for each user object will hold.
+        '''
+
+        # instance variables
+        self.first_name = first_name
+        self.last_name = last_name
         self.password = password
 
-    def register_user(self):
+    def save_user(self):
         '''
-        register_user method to register new user to the users_list
+        Function to save a newly created user instance
         '''
-        user_details = {self.username: self.password}
-        User.users_list.update(user_details)
+        User.users_list.append(self)
 
-    # @classmethod
-    # def delete_user(cls, username):
-    #     '''
-    #     delete_user method to delete a user from the users_list
-    #     '''
 
-    #     for key in User.users_list.keys():
-    #         if key == username:
-    #             del [User.users_list[key]]
+class Credential:
+    '''
+    Class to create  account credentials, generate passwords and save their information
+    '''
+    # Class Variables
+    credentials_list = []
+    user_credentials_list = []
+    @classmethod
+    def check_user(cls, first_name, password):
+        '''
+        Method that checks if the name and password entered match entries in the users_list
+        '''
+        current_user = ''
+        for user in User.users_list:
+            if (user.first_name == first_name and user.password == password):
+                current_user = user.first_name
+        return current_user
+
+    def __init__(self, user_name, site_name, account_name, password):
+        '''
+        Method to define the properties for each user object will hold.
+        '''
+
+        # instance variables
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.password = password
+
+    def save_credentials(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        # global users_list
+        Credential.credentials_list.append(self)
+
+    def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+        '''
+        Function to generate an 8 character password for a credential
+        '''
+        gen_pass = ''.join(random.choice(char) for _ in range(size))
+        return gen_pass
 
     @classmethod
-    def user_exist(cls, username):
+    def display_credentials(cls, user_name):
         '''
-        Method to check if a user exist in the users_list
+        Class method to display the list of credentials saved
+        '''
+        user_credentials_list = []
+        for credential in cls.credentials_list:
+            if credential.user_name == user_name:
+                user_credentials_list.append(credential)
+        return user_credentials_list
 
-        Args :
-            username : the username to search if it exists
+    @classmethod
+    def find_by_site_name(cls, site_name):
+        '''
+        Method that takes in a site_name and returns a credential that matches that site_name.
+        '''
+        for credential in cls.credentials_list:
+            if credential.site_name == site_name:
+                return credential
 
-        Returns :
-            Boolean : True or False depending if the user exists
+    @classmethod
+    def copy_credential(cls, site_name):
         '''
-        if username in cls.users_list:
-            return True
-        else :
-            return False
-
-
-class Credentials:
-    '''
-    Class that generates new instance of credentials
-    '''
-    credential_list = {}
-    platforms = []
-    
-    def __init__(self, account):
-        self.account = account
-    
-    def register_platform(self):
+        Class method that copies a credential's info after the credential's site name is entered
         '''
-         A method to register a new social media account to platforms
-        '''
-        Credentials.platforms.append(self)
-    
-    def delete_platform(self):
-        '''
-        A method to delet a social media platform
-        '''
+        find_credential = Credential.find_by_site_name(site_name)
         
-        Credentials.platforms.remove(self)
+        return pyperclip.copy(find_credential.password)
